8bf990938b7bbe1ace034241dd47ffded8232659
Merve K <32885177+mkabakci94@users.noreply.github.com>
2020-12-16 23:29:23 +0100

    update

--- CGAFramework/src/main/kotlin/cga/exercise/game/Scene.kt
+++ CGAFramework/src/main/kotlin/cga/exercise/game/Scene.kt
@@ -19,139 +19,174 @@ import org.joml.Vector3f
 import org.lwjgl.opengl.*
 import org.lwjgl.glfw.GLFW.*
 import org.lwjgl.opengl.GL11.*
-import org.lwjgl.opengl.GL15
+import org.lwjgl.opengl.GL13.*
+
+
 
 
 /**
  * Created by Fabian on 16.09.2017.
  */
-class Scene(private val window: GameWindow)  {
-     val staticShader: ShaderProgram
+class Scene(private val window: GameWindow) {
+
+    val staticShader: ShaderProgram
 
 
-    var bodenr :Renderable
+    var offset: Vector3f?
+    var rotate: Vector3f?
+    var scale: Vector3f?
+    var color: Vector3f?
+    var renderObject: Renderable? = null
+    var passedTime = 0.0f
 
-    var camera :TronCamera
-    var cubeCamera : TronCamera
 
-    var material :Material
-    var diff :Texture2D
-    var emit :Texture2D
-    var spec : Texture2D
-    var pointlight :PointLight
-    var spotligt :SpotLight
+
+    var bodenr: Renderable
+
+    var camera: TronCamera
+    var cubeCamera: TronCamera
+
+    var material: Material
+    var diff: Texture2D
+    var emit: Texture2D
+    var spec: Texture2D
+    var pointlight: PointLight
+    var spotligt: SpotLight
 
     //Elemente
     var cup: Renderable
     var cube: Renderable
     var tisch: Renderable
-    //var lamp : Renderable
 
-    var cam : Int = 0
-    var jumps = 0 //??
 
-    var xPosition : Double = 0.0
-    var yPosition : Double = 0.0
+    var shadowTexture: Int
+
+    var cam: Int = 0
+
+    var xPosition: Double = 0.0
+    var yPosition: Double = 0.0
 
     //scene setup
     init {
 
-
-        //Shader
-       // staticShader = ShaderProgram("assets/shaders/simple_vert.glsl", "assets/shaders/simple_frag.glsl")
-        staticShader = ShaderProgram("assets/shaders/tron_vert.glsl", "assets/shaders/tron_frag.glsl")
-        //staticShader = ShaderProgram("assets/shaders/tron_vert.glsl", "assets/shaders/tron_frag.glsl")
-
-
-        //cube würfel
-        cube = ModelLoader.loadModel("assets/models/cube.obj", Math.toRadians(0f), Math.toRadians(90f), 0f)
-            ?: throw IllegalArgumentException("Could not load the model cube")
-        cube.translateGlobal(Vector3f(1.5f, 0f, 3.2f))
-        cube.scaleLocal(Vector3f(0.005f, 0.005f, 0.005f))
+        val window = window
+        color = Vector3f(0.0f)
+        offset = Vector3f(0.0f)
+        rotate = Vector3f(0.0f)
+        scale = Vector3f(0.5f)
 
 
-        //cup tasse
-        cup = ModelLoader.loadModel("assets/models/cup.obj", Math.toRadians(0f), Math.toRadians(0f), 0f)
-            ?: throw IllegalArgumentException("Could not load the model Cup")
-        cup.translateGlobal(Vector3f(-4f, 1.3f, -2.8f))
-        cup.scaleLocal(Vector3f(0.1f, 0.1f, 0.1f))
+            //Shader
+            // staticShader = ShaderProgram("assets/shaders/simple_vert.glsl", "assets/shaders/simple_frag.glsl")
+            staticShader = ShaderProgram("assets/shaders/tron_vert.glsl", "assets/shaders/tron_frag.glsl")
+            //staticShader = ShaderProgram("assets/shaders/tron_vert.glsl", "assets/shaders/tron_frag.glsl")
 
 
-
-        //tisch
-        tisch = ModelLoader.loadModel("assets/models/tisch.obj",  Math.toRadians(0f), Math.toRadians(90f), 0f)
-            ?: throw IllegalArgumentException("Could not load the model tisch")
-        tisch.translateGlobal(Vector3f(-4f, 0f, -3f))
-        tisch.scaleLocal(Vector3f(0.03f, 0.03f, 0.03f))
-
-
-        //
-        val res2: OBJLoader.OBJResult = OBJLoader.loadOBJ("assets/models/ground.obj")
-        val objMesh2: OBJLoader.OBJMesh = res2.objects[0].meshes[0]
-
-        //Boden
-        diff = Texture2D.invoke("assets/textures/ground_diff.png",true)
-        diff.setTexParams(GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
-        emit = Texture2D.invoke("assets/textures/ground_emit.png",true)
-        emit.setTexParams(GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
-        spec = Texture2D.invoke("assets/textures/ground_spec.png",true)
-        spec.setTexParams(GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
+            //cube würfel
+            cube = ModelLoader.loadModel("assets/models/cube.obj", Math.toRadians(0f), Math.toRadians(90f), 0f)
+                ?: throw IllegalArgumentException("Could not load the model cube")
+            cube.translateGlobal(Vector3f(1.5f, 0f, 3.2f))
+            cube.scaleLocal(Vector3f(0.005f, 0.005f, 0.005f))
 
 
-       material = Material(diff,emit,spec,60f, Vector2f(64f,64f))
-
-        val stride: Int = 8 * 4
-        val attrPos = VertexAttribute(3, GL15.GL_FLOAT, stride, 0)
-        val attrTC =  VertexAttribute(2, GL15.GL_FLOAT, stride, 3 * 4)
-        val attrNorm =  VertexAttribute(3, GL15.GL_FLOAT, stride, 5 * 4)
-        val vertexAttributes = arrayOf(attrPos, attrTC, attrNorm)
-        //kugelMesh = Mesh(objMesh.vertexData, objMesh.indexData, vertexAttributes)
-        val bodenmesh  = Mesh(objMesh2.vertexData, objMesh2.indexData, vertexAttributes,material)
+            //cup tasse
+            cup = ModelLoader.loadModel("assets/models/cup.obj", Math.toRadians(0f), Math.toRadians(0f), 0f)
+                ?: throw IllegalArgumentException("Could not load the model Cup")
+            cup.translateGlobal(Vector3f(-4f, 1.3f, -2.8f))
+            cup.scaleLocal(Vector3f(0.1f, 0.1f, 0.1f))
 
 
-        bodenr = Renderable(mutableListOf(bodenmesh))
-        //kugelr = Renderable(mutableListOf(kugelMesh))
+            //tisch
+            tisch = ModelLoader.loadModel("assets/models/tisch.obj", Math.toRadians(0f), Math.toRadians(90f), 0f)
+                ?: throw IllegalArgumentException("Could not load the model tisch")
+            tisch.translateGlobal(Vector3f(-4f, 0f, -3f))
+            tisch.scaleLocal(Vector3f(0.03f, 0.03f, 0.03f))
 
 
-        //bodenr.rotateLocal(Math.toRadians(90.0f),0f,0f)
-        //bodenr.scaleLocal(Vector3f(0.03f,0.03f,0.03f))
-        // kugelr.scaleLocal(Vector3f(0.5f,0.5f,0.5f))
+            //
+            val res2: OBJLoader.OBJResult = OBJLoader.loadOBJ("assets/models/ground.obj")
+            val objMesh2: OBJLoader.OBJMesh = res2.objects[0].meshes[0]
 
-        //Kamera
-
-
-        camera = TronCamera()
-        camera.rotateLocal(0f,0f,0f)
-        camera.translateLocal(Vector3f(0f,0f,40f))
-        //camera.rotateLocal(Math.toRadians(-35.0f),0f,0f)
-        //camera.translateLocal(Vector3f(0.0f,0.0f,4.0f))
-       // camera.translateLocal(Vector3f(0.0f,400.0f,200.0f)) //Kamera position
-        camera.parent = cube
-
-        cubeCamera = TronCamera()
-        cubeCamera.translateLocal(Vector3f(0f,0f,60f))
-
-        //Licht (Position, Farbe)
-        pointlight = PointLight(Vector3f(0f,2f,0f),Vector3f(0f,1f,0f))
-        pointlight.parent = cube
+            //Boden
+            diff = Texture2D.invoke("assets/textures/ground_diff.png", true)
+            diff.setTexParams(GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
+            emit = Texture2D.invoke("assets/textures/ground_emit.png", true)
+            emit.setTexParams(GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
+            spec = Texture2D.invoke("assets/textures/ground_spec.png", true)
+            spec.setTexParams(GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR)
 
 
-        spotligt = SpotLight(Vector3f(0f,0f,0f),Vector3f(1f,0f,1f),Math.cos(Math.toRadians(30f)),Math.cos(Math.toRadians(50f)))
-        spotligt.parent = cube
+            material = Material(diff, emit, spec, 60f, Vector2f(64f, 64f))
+
+            val stride: Int = 8 * 4
+            val attrPos = VertexAttribute(3, GL_FLOAT, stride, 0)
+            val attrTC = VertexAttribute(2, GL_FLOAT, stride, 3 * 4)
+            val attrNorm = VertexAttribute(3, GL_FLOAT, stride, 5 * 4)
+            val vertexAttributes = arrayOf(attrPos, attrTC, attrNorm)
+            //kugelMesh = Mesh(objMesh.vertexData, objMesh.indexData, vertexAttributes)
+            val bodenmesh = Mesh(objMesh2.vertexData, objMesh2.indexData, vertexAttributes, material)
 
 
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); GLError.checkThrow()
-        glEnable(GL_DEPTH_TEST); GLError.checkThrow()
-        glDepthFunc(GL_LESS); GLError.checkThrow()
+            bodenr = Renderable(mutableListOf(bodenmesh))
+            //kugelr = Renderable(mutableListOf(kugelMesh))
 
 
-        glEnable(GL_CULL_FACE)
-        glFrontFace(GL_CCW)
-        glCullFace(GL_BACK)
+            //bodenr.rotateLocal(Math.toRadians(90.0f),0f,0f)
+            //bodenr.scaleLocal(Vector3f(0.03f,0.03f,0.03f))
+            // kugelr.scaleLocal(Vector3f(0.5f,0.5f,0.5f))
+
+            //Kamera
+            camera = TronCamera()
+            camera.rotateLocal(0f, 0f, 0f)
+            camera.translateLocal(Vector3f(0f, 0f, 40f))
+
+
+            cubeCamera = TronCamera()
+            cubeCamera.translateLocal(Vector3f(0f, 0f, 60f))
+             camera.parent = cube
+
+            //Licht (Position, Farbe)
+            pointlight = PointLight(Vector3f(0f, 2f, 0f), Vector3f(0f, 1f, 0f))
+            pointlight.parent = cube
 
 
 
-    }
+            spotligt = SpotLight(Vector3f(0f,0f,0f),Vector3f(1f,0f,1f),Math.cos(Math.toRadians(30f)),Math.cos(Math.toRadians(50f)))
+            spotligt.parent = cube
+
+
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f); GLError.checkThrow()
+            glEnable(GL_DEPTH_TEST); GLError.checkThrow()
+            glDepthFunc(GL_LESS); GLError.checkThrow()
+
+
+            glEnable(GL_CULL_FACE)
+            glFrontFace(GL_CCW)
+            glCullFace(GL_BACK)
+
+
+            shadowTexture = GL11.glGenTextures()
+            GL11.glBindTexture(GL11.GL_TEXTURE_2D, shadowTexture)
+            GL11.glTexImage2D(
+                GL11.GL_TEXTURE_2D,
+                0,
+                GL_DEPTH_COMPONENT,
+                1024,
+                1024,
+                0,
+                GL11.GL_DEPTH_COMPONENT,
+                GL11.GL_FLOAT,
+                0
+            )
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST)
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST)
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER)
+            GL11.glTexParameterfv(GL_TEXTURE_2D, GL11.GL_TEXTURE_BORDER_COLOR, floatArrayOf(1.0f, 1.0f, 1.0f, 1.0f))
+
+        }
+
+
 
     fun render(dt: Float, t: Float) {
 
@@ -164,15 +199,38 @@ class Scene(private val window: GameWindow)  {
         spotligt.bind(staticShader," ", camera.getCalculateViewMatrix())
         staticShader.setUniform("shadingcolor",Vector3f(0f,1f,0f))
         bodenr.render(staticShader)
-        //cycle.render(staticShader)
 
         cube.render(staticShader)
         cup.render(staticShader)
         tisch.render(staticShader)
 
+       // GL30.glBindVertexArray(0)
+        //GLError.checkExit()
+
+
+
     }
 
     fun update(dt: Float, t: Float) {
+
+
+        // für die automatische bewegung des affens
+        passedTime++
+        offset!!.x += 0.001f
+        rotate!!.y += 0.01f
+        rotate!!.z += 0.01f
+        scale!!.x += 0.001f
+        scale!!.y += 0.001f
+
+        //renderObject.translateOBJ(offset)
+        //renderObject.rotateOBJ(rotate)
+        //renderObject.scaleOBJ(scale)
+
+        renderObject!!.translateLocal(Vector3f(0.0f,0.0f,-5.0f*dt))
+        renderObject!!.rotateLocal(0.0f,-2f*dt,0.0f)
+       // renderObject!!.scaleLocal(0.0f)
+
+        //Tasten
 
         //vorwärts
         if(window.getKeyState(GLFW_KEY_W)){
@@ -208,15 +266,18 @@ class Scene(private val window: GameWindow)  {
 
 
 
+
     }
 
     fun onKey(key: Int, scancode: Int, action: Int, mode: Int) {}
 
 
-    var alt_xpos: Double = 0.0
-    var alt_ypos: Double = 0.0
+
 
     fun onMouseMove(xpos: Double, ypos: Double) {
+
+        var alt_xpos: Double = 0.0
+        var alt_ypos: Double = 0.0
 
         cubeCamera.rotateLocal(Math.toRadians((-ypos + yPosition ).toFloat() * 0.02f), Math.toRadians((-xpos + xPosition ).toFloat() * 0.02f),0f )
         camera.rotateLocal(Math.toRadians((-ypos + yPosition ).toFloat() * 0.02f), Math.toRadians((-xpos + xPosition ).toFloat() * 0.02f),0f )
@@ -224,14 +285,14 @@ class Scene(private val window: GameWindow)  {
         yPosition = ypos
 
 
+        //vorgegeben
         val diff_x = alt_xpos - xpos
         val diff_y = alt_ypos - ypos
 
-      // vorgegeben
-         var olpx =0.0
+        var olpx =0.0
         var oldpy =0.0
 
-        //Maus/Kamera um die Spielfigur bewegen
+        //Kamera um die Spielfigur bewegen
         camera.rotateAroundPoint(0f, diff_x.toFloat() * 0.001f, 0f, cube.getYAxis())
         //camera.rotateAroundPoint(diff_y.toFloat() * 0.001f, 0f, 0f, cat.getXAxis())
 
